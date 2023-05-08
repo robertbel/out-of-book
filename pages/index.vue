f8a880aaec8bf97e486dc89155af18423e6436a2
robertbel <r.bellekom@gmail.com>
2023-05-08 07:12:33 +0200

    First test for live updates

--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,32 @@
 </template>
 
 <script setup>
-const { data: gamesRes } = useFetch('/api/get-games')
+const { data: gamesRes } = useFetch('/api/get-games');
 const { data: openingsRes } = useFetch('/api/get-openings')
 
-const data = gamesRes || {}
+const data = gamesRes || {};
 const lala = openingsRes || {}
+
+const getGames = async () => {
+  console.log('Called!');
+  const result = await fetch('/api/get-games');
+  const newData = await result.json();
+  data.chessGames = newData.chessGames;
+}
+
+onMounted(() => {
+  watchEffect(async () => {
+    const intervalId = setInterval(() => {
+      getGames();
+    }, 2000);
+
+    onUnmounted(() => {
+      clearInterval(intervalId);
+    });
+  });
+});
 </script>
+
 
 <style>
 .main {
