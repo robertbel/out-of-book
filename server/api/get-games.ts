e859e929a31b0456624a43da497ac73c4e18ca0d
robertbel <r.bellekom@gmail.com>
2023-05-08 09:11:41 +0200

    Log to see if query is called

--- server/api/get-games.ts
+++ server/api/get-games.ts
@@ -1,6 +1,7 @@
 import { createPool } from '@vercel/postgres';
 
 export default defineEventHandler(async () => {
+  console.log('Getting games from database');
   const db = createPool();
   try {
     const { rows: chessGames } = await db.query('SELECT * FROM chess_games ORDER BY id DESC');
