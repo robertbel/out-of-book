6834f302605779798fc4dcee60979f51e9274e61
robertbel <r.bellekom@gmail.com>
2023-05-13 16:13:22 +0200

    Added Pgn parser

--- components/Table.vue
+++ components/Table.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="games">
     <div v-for="game in games" :key="game.id" class="game">
-      <TheChessboard :board-config="{ ...boardConfig, orientation: game?.orientation }" @board-created="(api) => (loadPgn(api, game?.game_data.pgn))" />
+      <TheChessboard :board-config="{ ...boardConfig, orientation: game?.orientation }" @board-created="(api) => (loadPgn(api, game?.game_data.pgn, game?.game_id))" />
       <div class="pgn">
         <p>Id: {{ game?.game_id }}</p>
         <p>Color: {{ game?.orientation }}</p>
-        <pre>{{ game?.game_data.pgn }}</pre>
+        <pre>{{ notations[game?.game_id] }}</pre>
       </div>
-
     </div>
   </div>
 </template>
 
 <script setup>
+import { parse } from '@mliebelt/pgn-parser';
 import { TheChessboard } from 'vue3-chessboard';
 import 'vue3-chessboard/style.css';
 
@@ -31,8 +31,20 @@ const boardConfig = {
   coordinates: true,
 };
 
-const loadPgn = (api, pgn) => {
+const parsedGames = ref({});
+
+const notations = computed(() => {
+  const notations = {};
+  for (const game in parsedGames.value) {
+    notations[game] = parsedGames.value[game].moves.map(move => move.notation.notation);
+  }
+  return notations;
+});
+
+const loadPgn = (api, pgn, gameId) => {
   api.loadPgn(pgn);
+  const game = parse(pgn, { startRule: "game" });
+  parsedGames.value[gameId] = game;
 };
 </script>
 
