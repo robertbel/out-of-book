5bdc817c64cbd64aaeaf90c05f1413b507d962a4
robertbel <r.bellekom@gmail.com>
2023-05-13 14:37:29 +0200

    Update get-played-games.js

--- server/api/get-played-games.js
+++ server/api/get-played-games.js
@@ -1,53 +1,58 @@
 import axios from 'axios';
 import { sql } from '@vercel/postgres';
 
+const CHESS_API_BASE = 'https://api.chess.com/pub';
+const USERNAME = process.env.CHESS_USERNAME || 'luffyyyyyyyy'; // Fallback to 'luffyyyyyyyy' if not set in environment
+
+function determineOrientation(game, username) {
+  if (game.white.username.toLowerCase() === username.toLowerCase()) {
+    return "white";
+  } else if (game.black.username.toLowerCase() === username.toLowerCase()) {
+    return "black";
+  }
+
+  console.log(`Unable to determine orientation for game: ${game.uuid}`);
+  return null;
+}
+
+async function insertGame(game, username) {
+  const gameId = game.uuid;
+  const jsonData = JSON.stringify(game);
+  const orientation = determineOrientation(game, username);
+
+  if (orientation) {
+    await sql`
+      INSERT INTO chess_games (game_id, game_data, orientation)
+      VALUES (${gameId}, ${jsonData}, ${orientation})
+      ON CONFLICT (game_id) DO NOTHING;
+    `;
+  }
+}
+
 async function storeGameData(gameData, username) {
   try {
     const games = gameData.games;
-
     for (const game of games) {
-      const gameId = game.uuid;
-      const jsonData = JSON.stringify(game);
-
-      // Determine the orientation
-      let orientation = null;
-      if (game.white.username.toLowerCase() === username.toLowerCase()) {
-        orientation = "white";
-      } else if (game.black.username.toLowerCase() === username.toLowerCase()) {
-        orientation = "black";
-      }
-
-      if (orientation) {
-        await sql`
-          INSERT INTO chess_games (game_id, game_data, orientation)
-          VALUES (${gameId}, ${jsonData}, ${orientation})
-          ON CONFLICT (game_id) DO NOTHING;
-        `;
-      } else {
-        console.log(`Unable to determine orientation for game: ${gameId}`);
-      }
+      await insertGame(game, username);
     }
-
     console.log('Game data stored successfully');
   } catch (error) {
     console.error('Error storing game data:', error);
   }
 }
 
-
 export default defineEventHandler(async () => {
-  const username = 'luffyyyyyyyy';
   const currentDate = new Date();
   const currentYear = currentDate.getFullYear();
   const currentMonth = String(currentDate.getMonth() + 1).padStart(2, '0');
-  const url = `https://api.chess.com/pub/player/${username}/games/${currentYear}/${currentMonth}`;
+  const url = `${CHESS_API_BASE}/player/${USERNAME}/games/${currentYear}/${currentMonth}`;
 
   try {
     const response = await axios.get(url);
     const data = response.data;
 
     if (data) {
-      await storeGameData(data, username);
+      await storeGameData(data, USERNAME);
     }
 
     return {
