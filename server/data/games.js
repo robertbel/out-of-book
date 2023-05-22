1d69de3b9ebfa8553e246af5e9be0c8f3da296a8
robertbel <r.bellekom@gmail.com>
2023-05-22 15:06:51 +0200

    Prisma file

--- server/data/games.js
+++ server/data/games.js
@@ -1,5 +1,4 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import prisma from './prisma'
 
 export async function getGames() {
   const games = await prisma.games.findMany({
