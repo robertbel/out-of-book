c7bf9b30d1deb70825303bca699b316b2f50d97e
robertbel <r.bellekom@gmail.com>
2023-05-22 16:30:08 +0200

    Go again

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@ const { data: games } = await useFetch('/api/games');
 
 <template>
   <nav>
-    <NuxtLink to="/about">About us</NuxtLink>
+    <NuxtLink to="/about">About</NuxtLink>
   </nav>
   <main class="main">
     <Table v-if="games" :games="games" />
