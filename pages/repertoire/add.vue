3e39b0c6804040158e2f3489ff4f5ce4de40e730
robertbel <r.bellekom@gmail.com>
2023-05-26 12:53:12 +0200

    Add repertoire - flip works & start of pgn generation with comments

--- pages/repertoire/add.vue
+++ pages/repertoire/add.vue
@@ -2,21 +2,24 @@
   <h1>Add repertoire</h1>
   <div class="board">
     <div class="chessboard">
-      {{ orientation }}
       <client-only>
-        <TheChessboard @move="handleMove" :board-config="boardConfig" />
+        <TheChessboard @board-created="(api) => (boardAPI = api)" @move="mov(boardAPI)" />
         <template #placeholder>
           <ChessboardLoader />
         </template>
       </client-only>
+      <div class="notes">
+        <input type="text" v-model="note"><button @click="com(boardAPI, note)">Add</button>
+      </div>
     </div>
     <aside class="side">
       <div class="moves">
-        <ul>
-          <li v-for="move in moves">{{ move.san }}</li>
-        </ul>
+        {{ currPgn }}
       </div>
-      <div class="options" @click="flipBoard">Flip board</div>
+      <div class="options">
+        <div class="played-as">Played as: {{ playedAs }}</div>
+        <div class="flip" @click="flip(boardAPI)">Flip board</div>
+      </div> 
     </aside>
   </div>
 </template>
@@ -25,39 +28,27 @@
 import { TheChessboard } from 'vue3-chessboard';
 import 'vue3-chessboard/style.css';
 
-const orientation = ref('white');
-const moves = ref([]);
-const boardConfig = ref({
-  coordinates: true,
-  orientation: orientation.value,
-  events: {
-    change: () => {
-      // called after the situation changes on the board
-      console.log('Something changed!');
-    },
-    move: (from, to, capture) => {
-      // the move function fires after each move on the board, you can access the values from, to, and capture
-      console.log(from, to, capture);
-    },
-    select(key) {
-      // called when a square is selected
-      // the param key is the square being selected: e.g. d2 or e2...
-      console.log(key);
-    },
-  },
-});
+const playedAs = ref('white');
+const boardAPI = ref();
+const currPgn = ref();
+const note = ref();
 
-function handleMove(move) {
-  moves.value.push(move);
-  console.log(move);
+function mov(api) {
+  currPgn.value = api.getPgn();
 }
 
-function flipBoard() {
-  if (orientation.value === 'white') {
-    orientation.value = 'black';
+function flip(api) {
+  api.toggleOrientation();
+  if(playedAs.value === 'white') {
+    playedAs.value = 'black';
   } else {
-    orientation.value = 'white';
+    playedAs.value = 'white';
   }
+}
+
+function com(api, comment) {
+  api.game.setComment(comment)
+  currPgn.value = api.getPgn();
 }
 </script>
 
