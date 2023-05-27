d8308b63d661400a49a5c344fc322dd281f9bf93
robertbel <r.bellekom@gmail.com>
2023-05-27 13:59:24 +0200

    Start listing moves

--- pages/repertoire/add.vue
+++ pages/repertoire/add.vue
@@ -3,7 +3,7 @@
   <div class="board">
     <div class="chessboard">
       <client-only>
-        <TheChessboard @board-created="(api) => (boardAPI = api)" @move="updatePgn(boardAPI)" />
+        <TheChessboard @board-created="(api) => (boardAPI = api)" @move="moveMade(boardAPI)" />
         <template #placeholder>
           <ChessboardLoader />
         </template>
@@ -13,31 +13,36 @@
       </div>
     </div>
     <aside class="side">
-      <div class="moves">
-        {{ currentPgn }}
+      <div class="moves" v-if="moves">
+        <div v-for="move of moves[0].moves" class="move">
+          {{ move.notation.notation }} <span v-if="move.commentAfter">{{ move.commentAfter }}</span>
+        </div>
       </div>
       <div class="options">
         <div class="played-as">Played as: {{ playedAs }}</div>
         <div class="flip" @click="flipBoard(boardAPI)">Flip board</div>
-      </div> 
+      </div>
     </aside>
   </div>
 </template>
 
 <script setup>
+import { parse } from '@mliebelt/pgn-parser'
 import { TheChessboard } from 'vue3-chessboard';
 import 'vue3-chessboard/style.css';
 
 const playedAs = ref('white');
 const boardAPI = ref();
 const currentPgn = ref();
+const moves = ref();
 const moveComment = ref();
 
-function updatePgn(api) {
+function moveMade(api) {
   currentPgn.value = api.getPgn();
+  moves.value = parse(currentPgn.value);
 }
 
-function flipBoard(api, playedAs) {
+function flipBoard(api) {
   try {
     api.toggleOrientation();
 
@@ -50,16 +55,16 @@ function flipBoard(api, playedAs) {
 function addCommentToMove(api, comment) {
   api.game.setComment(comment);
   moveComment.value = '';
-  currentPgn.value = api.getPgn();
+  moveMade(api);
 }
 </script>
 
 <style scoped>
 .board {
   margin-inline: auto;
-  max-width: 700px;
+  max-width: 900px;
   display: grid;
-  grid-template-columns: 1fr 10rem;
+  grid-template-columns: 1fr 20rem;
   gap: 1rem;
 }
 
@@ -71,5 +76,15 @@ function addCommentToMove(api, comment) {
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+}
+
+.moves {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+
+.move {
+  padding-block: 1rem;
+  border-bottom: 1px solid #eee;
 }
 </style>
