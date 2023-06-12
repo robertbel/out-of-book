f9b726cf28f34cc3789c3da94f47389ca083f7e8
robertbel <r.bellekom@gmail.com>
2023-06-12 13:09:52 +0200

    Make sure we have same format for moves in games and repertoires

--- components/Table.vue
+++ components/Table.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="games">
-    <div v-for="game in games" :key="game.id">
+    <div v-for="(game, index) in games" :key="index">
       <div v-if="game.deviation" class="game">
         <client-only>
-          <NuxtLink :to="`/repertoire/${game.id}`" @click.native="active = game.id">
-            <TheChessboard class="beebee" :class="{ active: active === game.id }" :board-config="{ ...boardConfig, orientation: game?.played_as === 'w' ? 'white' : 'black' }" @board-created="(api) => (loadFen(api, game?.deviation.fen_notation))" />
+          <NuxtLink
+            :to="`/repertoire/${game.id}`"
+            @click.native="active = game.id"
+          >
+            <TheChessboard
+              class="beebee"
+              :class="{ active: active === game.id }"
+              :board-config="{
+                ...boardConfig,
+                orientation: game?.played_as === 'w' ? 'white' : 'black',
+              }"
+              @board-created="
+                (api) => loadFen(api, game?.deviation.fen_notation, game?.pgn)
+              "
+            />
           </NuxtLink>
           <template #placeholder>
             <ChessboardLoader />
@@ -12,10 +25,19 @@
         </client-only>
         <div class="pgn">
           <div>
-            ID: <NuxtLink :to="`/repertoire/${game.id}`" @click.native="active = game.id">{{ game?.id }}</NuxtLink>
+            ID:
+            <NuxtLink
+              :to="`/repertoire/${game.id}`"
+              @click.native="active = game.id"
+              >{{ game?.id }}</NuxtLink
+            >
           </div>
-          <div v-if="game.deviation">Deviation in <strong>{{ game.repertoires?.repertoire_name }}</strong> repertoir on move <strong>{{ game.deviation }}</strong></div>
-          <!-- <pre>{{ game }}</pre> -->
+          <div v-if="game.deviation">
+            Deviation in
+            <strong>{{ game.repertoires?.repertoire_name }}</strong> repertoir
+            on move <strong>{{ game.deviation }}</strong>
+          </div>
+          <pre>{{ history[index] }}</pre>
         </div>
       </div>
     </div>
@@ -23,8 +45,8 @@
 </template>
 
 <script setup>
-import { TheChessboard } from 'vue3-chessboard';
-import 'vue3-chessboard/style.css';
+import { TheChessboard } from "vue3-chessboard";
+import "vue3-chessboard/style.css";
 
 const active = useState();
 
@@ -32,22 +54,25 @@ const props = defineProps({
   games: {
     type: Array,
     required: true,
-  }
-})
+  },
+});
+
+const history = ref([]);
 
 const boardConfig = {
   coordinates: true,
   viewOnly: true,
   animation: {
-    enabled: false
-  }
+    enabled: false,
+  },
 };
 
-const loadFen = (api, fen) => {
-  api.setPosition(fen);
+const loadFen = (api, fen, pgn) => {
+  api.loadPgn(pgn);
+  history.value.push(api.getHistory({ verbose: true }));
+  api.setPosition(fen); //Position of deviation
 };
 </script>
-
 
 <style scoped>
 .beebee.active {
